13002b473531ce3241d40e8ffc098ce3bf4eb60f
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-04-06 06:50:39 +0530

    added examples and use cases

--- app.ts
+++ app.ts
@@ -1,83 +1,90 @@
-class Department {
-    private readonly id: string;
-    public name: string; // 'public' access modifier is default.
-    protected employees: string[] = [];
+// * Intersection types
+// * Intersection in union type
+type typeA = string | number;
+type typeB = number | boolean;
+type typeAB = typeA & typeB;
 
-    constructor(id: string, n: string, ) {
-        this.name = n;
-        this.id = id;
-    }
+let intersectionType: typeAB = 123;
 
-    // sborthand syntax to initialize variable in class.
-    // constructor(public name: string){
-
-    // }
-
-    describe(this: Department) {
-        console.log("Department: ", this.name);
-    }
-
-    addEmployee(employee: string) {
-        this.employees.push(employee);
-    }
+// * Intersection in object type
+interface BusinessPartner {
+    name: string;
+    credit: number;
 }
 
-// Inheriting 'Department' class
-class ITDepartment extends Department {
-    admins: string[];
-    static employeeCount: number;
-
-    constructor(id: string,  admins: string[]) {
-        super(id, "IT");
-        this.admins = admins;
-    }
-
-    // Override method of class 'Department`'
-    addEmployee(employee: string) {
-        if (employee === 'David') {
-            return;
-        }
-        this.employees.push(employee);
-    }
-
-    // Getter method
-    get getAllAdmins() : string[] {
-        return this.admins;
-    }
-
-    // Setter method
-    set addAdmin(admin: string) {
-        this.admins.push(admin);
-    }
-
-    static getEmployeeCount() {
-        console.log(this.employeeCount);
-    }
+interface Identity {
+    id: number;
+    name: string;
+    aadharNumber: number
 }
 
-// Create an object from defined class above. 
-let accounting = new Department('d1', 'Accounting');
+interface Contact {
+    email: string;
+    phone: string;
+}
 
-accounting.addEmployee("Adam");
+type Employee = Identity & Contact; // contains all fields from Identity and Contact
 
-let it = new ITDepartment('d2', ['David']);
-console.log(it);
+let employee: Employee = {
+    id: 201,
+    name: "John Doe",
+    email: "john.doe@email.com",
+    phone: "1234567890",
+    aadharNumber: 1234567890
+}
 
-// Execute a getter method
-console.log(it.getAllAdmins);
+// * Type Casting
+// const paragraph = document.querySelector('p'); // TypeScript knows it is a <p> tag
+// const paragraph = document.getElementById('error-message'); // TypeScript doesnot know if it is a <p> tag
+const userInputElement = document.getElementById('user-input')!; // TypeScript doesnot know it is a <input> tag
+userInputElement.value = "Hello from TypeScript"; // error
+// above error can be solved with Type Casting
 
-// Execute a setter method
-it.addAdmin = "Alice";
+const userInputElement1 = <HTMLInputElement>document.getElementById('user-input')!;
+userInputElement1.value = "Hello from TypeScript";
 
-abstract class Employee {
-    employeeName: string;
-    age: number;
+const userInputElement2 = document.getElementById('user-input')! as HTMLInputElement; // Used in React world
+userInputElement2.value = "Hello from TypeScript";
 
-    constructor(employeeName: string, age: number) {
-        this.employeeName = employeeName;
-        this.age = age;
+const userInputElement3 = document.getElementById('user-input'); // Removing exclamation mark (null check)
+if(userInputElement3) {
+    (userInputElement3 as HTMLInputElement).value = "Hello from TypeScript";
+}
+
+// * Index Properties
+interface ErrorContainer {
+    // id: number; // Cannot be number
+    id: string;
+    [prop: string]: string
+}
+
+// * Function overloading
+type Combinable = string | number;
+
+function add(a: string, b: string): string; // Overload function (must be just before function implementation)
+function add(a: number, b: number): number;
+function add(a: Combinable, b: Combinable) {
+    if(typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
     }
+    return a + b;
+}
 
-    // This will be defined in a class that inherits this class
-    abstract get employeeDetails(): object;
+let result = add('Hello','World');
+result.split(' '); // TypeScript knows return type is either number or string and therefore gives an error.
+
+// Solution 1: Type casting
+let result1 = add('Hello', 'World') as string;
+result1.split(' ');
+
+// Solution 2: Function overloading (see above function implementation)
+let result2 = add('Hello', 'World');
+result2.split(' ');
+
+// Nullish coalesing operator use case
+// In the below example, when localStorage.volume is set to 0, the page will set the volume to 0.5 which is unintended.
+// nullinsh coalescing operator (??) avoids some unintended behavior from 0, NaN and "" being treated as falsy values.
+function initializeAudio() {
+    let volume = localStorage.volume || 0.5;
+    // ...
 }
