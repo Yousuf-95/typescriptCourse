962b127efb18ed6b60db7d26d5c83e44d7a765ef
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-04-27 22:57:22 +0530

    add state management

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,40 @@
+// Project State Management
+
+class ProjectState {
+   private listeners: any[] = [];
+   private projects: any[] = [];
+   private static instance: ProjectState;
+
+   private constructor() { }
+
+   static getInstance() {
+      if (this.instance) {
+         return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+   }
+
+   addListener(listenerFn: Function) {
+      this.listeners.push(listenerFn);
+   }
+
+   addProject(title: string, description: string, numOfPeople: number) {
+      const newProject = {
+         id: Math.random().toString(),
+         title: title,
+         description: description,
+         people: numOfPeople
+      };
+      this.projects.push(newProject);
+      for (const listenerFn of this.listeners) {
+         listenerFn(this.projects.slice());
+      }
+   }
+}
+ 
+ const projectState = ProjectState.getInstance();
+
 // Validation
 interface Validatable {
    value: string | number;
