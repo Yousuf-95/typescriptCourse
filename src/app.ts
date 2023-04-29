f8b46e9749297d0141e8cb1a2ae8d3d3390db835
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-04-29 23:03:52 +0530

    filter projects

--- src/app.ts
+++ src/app.ts
@@ -192,24 +192,49 @@ class ProjectList {
    templateElement: HTMLTemplateElement;
    hostElement: HTMLDivElement;
    element: HTMLElement;
+   assignedProjects: Project[];
 
    constructor(private type: 'active' | 'finished') {
       this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
       this.hostElement = document.getElementById('app')! as HTMLDivElement;
+      this.assignedProjects = [];
 
       const importedNode = document.importNode(this.templateElement.content, true);
       this.element = importedNode.firstElementChild as HTMLElement;
       this.element.id = `${this.type}-projects`;
+
+      projectState.addListener((projects: Project[]) => {
+         const relevantProjects = projects.filter(prj => {
+            if (this.type === 'active') {
+               return prj.status === ProjectStatus.Active;
+            }
+            return prj.status === ProjectStatus.Finished;
+         });
+         this.assignedProjects = relevantProjects;
+         this.renderProjects();
+      });
+
       this.attach();
       this.renderContent();
+   }
+
+   private renderProjects() {
+      const listEl = document.getElementById(
+         `${this.type}-projects-list`
+      )! as HTMLUListElement;
+      listEl.innerHTML = '';
+      for (const prjItem of this.assignedProjects) {
+         const listItem = document.createElement('li');
+         listItem.textContent = prjItem.title;
+         listEl.appendChild(listItem);
+      }
    }
 
    private renderContent() {
       const listId = `${this.type}-projects-list`;
       this.element.querySelector('ul')!.id = listId;
       this.element.querySelector('h2')!.textContent =
-         this.type.toUpperCase() + ' PROJECTS';
-
+      this.type.toUpperCase() + ' PROJECTS';
    }
 
    private attach() {
