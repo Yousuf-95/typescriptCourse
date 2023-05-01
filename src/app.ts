a683fe25a3c903199a12782e819f3303fcd93bd7
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-05-01 22:52:09 +0530

    render project list

--- src/app.ts
+++ src/app.ts
@@ -248,6 +248,30 @@ class ProjectList {
    }
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+   private project: Project;
+ 
+   constructor(hostId: string, project: Project) {
+     super('single-project', hostId, false, project.id);
+     this.project = project;
+ 
+     this.configure();
+     this.renderContent();
+   }
+ 
+   configure() {}
+ 
+   renderContent() {
+     this.element.querySelector('h2')!.textContent = this.project.title;
+     this.element.querySelector(
+       'h3'
+     )!.textContent = this.project.people.toString();
+     this.element.querySelector('p')!.textContent = this.project.description;
+   }
+ }
+ 
+
 const prjInput = new ProjectInput();
 const activePrjList = new ProjectList('active');
 const finishedPrjList = new ProjectList('finished');
