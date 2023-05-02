6b07f157deffb6bc42a04c7414b04a860a966db2
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-05-02 23:45:10 +0530

    define interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+interface Draggable {
+   dragStartHandler(event: DragEvent): void;
+   dragEndHandler(event: DragEvent): void;
+ }
+ 
+ interface DragTarget {
+   dragOverHandler(event: DragEvent): void;
+   dropHandler(event: DragEvent): void;
+   dragLeaveHandler(event: DragEvent): void;
+ }
+
 // Project Type
 enum ProjectStatus {
    Active,
